fd32c28fe03967118f403fd08ec19181df6f1fb9
Morteza <mortezamirzai120@gmail.com>
2019-05-18 20:22:53 +0430

    Complite AbstractEmployee Class.

--- src/org/university/core/person/employee/AbstractEmployee.java
+++ src/org/university/core/person/employee/AbstractEmployee.java
@@ -1,4 +1,34 @@
 package org.university.core.person.employee;
 
-public class AbstractEmployee {
+import org.university.core.person.Statement;
+
+import java.util.LinkedList;
+
+public abstract class AbstractEmployee {
+    private LinkedList<Statement> bankStatement;
+    private String position;
+    protected double basicIncome;
+
+    public AbstractEmployee(String position, double basicIncome) {
+        this.position = position;
+        this.basicIncome = basicIncome;
+
+        bankStatement = new LinkedList<>();
+    }
+
+    public void addBankStatement(Statement statement) {
+        bankStatement.add(statement);
+    }
+
+    public Statement[] getBankStatement() {
+        return bankStatement.toArray(new Statement[0]);
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setBasicIncome(double basicIncome) {
+        this.basicIncome = basicIncome;
+    }
 }
