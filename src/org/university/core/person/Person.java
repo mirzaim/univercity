10dfc6fb99904cc072fca86229b9a21ec6b10331
Morteza <mortezamirzai120@gmail.com>
2019-05-18 20:15:19 +0430

    Complite Person Class.

--- src/org/university/core/person/Person.java
+++ src/org/university/core/person/Person.java
@@ -7,4 +7,27 @@ public class Person {
     protected int joiningYear;
     protected Department department;
 
+    public Person(String firstName, String lastName, String id, int joiningYear, Department department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.joiningYear = joiningYear;
+        this.department = department;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getJoiningYear() {
+        return joiningYear;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
 }
