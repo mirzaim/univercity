2e7096b7e2f9ec415470ef3073400fdf842203f1
Morteza <mortezamirzai120@gmail.com>
2019-05-19 18:10:23 +0430

    Implement Publication class.

--- src/org/university/core/person/Publication.java
+++ src/org/university/core/person/Publication.java
@@ -1,4 +1,19 @@
 package org.university.core.person;
 
 public class Publication {
+    private String title;
+    private int year;
+
+    public Publication(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
