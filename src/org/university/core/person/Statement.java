11ad670818509ee94d252d2863ed9bb2a2953075
Morteza <mortezamirzai120@gmail.com>
2019-05-19 18:13:46 +0430

    Implement Statement class.

--- src/org/university/core/person/Statement.java
+++ src/org/university/core/person/Statement.java
@@ -1,4 +1,19 @@
 package org.university.core.person;
 
-public class Statement {
+public final class Statement {
+    private double amount;
+    private Person receiver;
+
+    public Statement(double amount, Person receiver) {
+        this.amount = amount;
+        this.receiver = receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Person getReceiver() {
+        return receiver;
+    }
 }
